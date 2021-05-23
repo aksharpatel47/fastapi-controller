dd1d2d03c66a2c3a6f657e1b92255d69f16a9b15
Akshar Patel <akshar.patel.47@gmail.com>
2021-05-23 00:26:46 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="fastapi-controller",
-    version="0.0.2",
+    version="0.0.3",
     packages=["fastapi_controller"],
     install_requires=[
         "fastapi-utils>=0.2.1",
